f6de31f5aebc3f32faa4d22f07db66e894124312
TU_USUARIO <TU_MAIL>
2025-08-30 21:33:53 -0300

    Puerto dinámico para Render

--- app.py
+++ app.py
@@ -30,5 +30,8 @@ def descargas():
 def download(filename):
     return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port, debug=True)
